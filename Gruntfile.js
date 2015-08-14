49149d0fa7a910ab7b85103301d964e42aecbbe2
Matthew Berryman <mberryma@uow.edu.au>
2015-08-14 15:11:04 +1000

    robots file
    Disallow API calls for robots

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,7 +91,8 @@ module.exports = function(grunt) {
         files: [
           { expand: true, flatten: true, src: "banjir/assets/img/*", dest: "build/banjir/img/" },
           { expand: true, flatten: true, src: "banjir/vendor/css/images/*", dest: "build/banjir/css/images/" },
-          { expand: true, flatten: true, src: "banjir/vendor/fonts/*", dest: "build/banjir/fonts/"}
+          { expand: true, flatten: true, src: "banjir/vendor/fonts/*", dest: "build/banjir/fonts/"},
+          { expand: true, flatten: true, src: "banjir/robots.txt", dest: "build/banjir/"}
         ]
       }
     },
