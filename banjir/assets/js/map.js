2caad6735e374256aea478bf0fb92d6ab0b45064
Tomas <tomas@uow.edu.au>
2016-07-04 17:02:28 +1000

    prototype sensor graph

--- banjir/assets/js/map.js
+++ banjir/assets/js/map.js
@@ -701,7 +701,50 @@ petajakarta.loadSensors = function(data){
 	petajakarta.sensors = L.geoJson(data, {
 		pointToLayer: function(feature, latlng) {
 			return L.marker(latlng, {icon:icon});
-		}
+		},
+		onEachFeature: function(feature, layer){
+			layer.on('click', function(e){
+				$('#sensorModal').modal('show').on('sshown.bs.modal', function (event) {
+				var ctx = $("#sensorChart").get(0).getContext("2d");
+				var myChart = new Chart(ctx, {
+							type: 'bar',
+							data: {
+									labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+									datasets: [{
+											label: '# of Votes',
+											data: [12, 19, 3, 5, 2, 3],
+											backgroundColor: [
+													'rgba(255, 99, 132, 0.2)',
+													'rgba(54, 162, 235, 0.2)',
+													'rgba(255, 206, 86, 0.2)',
+													'rgba(75, 192, 192, 0.2)',
+													'rgba(153, 102, 255, 0.2)',
+													'rgba(255, 159, 64, 0.2)'
+											],
+											borderColor: [
+													'rgba(255,99,132,1)',
+													'rgba(54, 162, 235, 1)',
+													'rgba(255, 206, 86, 1)',
+													'rgba(75, 192, 192, 1)',
+													'rgba(153, 102, 255, 1)',
+													'rgba(255, 159, 64, 1)'
+											],
+											borderWidth: 1
+									}]
+							},
+							options: {
+									scales: {
+											yAxes: [{
+													ticks: {
+															beginAtZero:true
+													}
+											}]
+									}
+							}
+					});
+				});
+				});
+			}
 	});
 
 	return petajakarta.sensors;
