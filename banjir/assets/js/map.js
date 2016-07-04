299c851d0b44c8050d7fbb166201af79575ec564
Tomas <tomas@uow.edu.au>
2016-07-04 17:11:10 +1000

    working template sensor graph

--- banjir/assets/js/map.js
+++ banjir/assets/js/map.js
@@ -704,45 +704,23 @@ petajakarta.loadSensors = function(data){
 		},
 		onEachFeature: function(feature, layer){
 			layer.on('click', function(e){
-				$('#sensorModal').modal('show').on('sshown.bs.modal', function (event) {
-				var ctx = $("#sensorChart").get(0).getContext("2d");
-				var myChart = new Chart(ctx, {
-							type: 'bar',
-							data: {
-									labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-									datasets: [{
-											label: '# of Votes',
-											data: [12, 19, 3, 5, 2, 3],
-											backgroundColor: [
-													'rgba(255, 99, 132, 0.2)',
-													'rgba(54, 162, 235, 0.2)',
-													'rgba(255, 206, 86, 0.2)',
-													'rgba(75, 192, 192, 0.2)',
-													'rgba(153, 102, 255, 0.2)',
-													'rgba(255, 159, 64, 0.2)'
-											],
-											borderColor: [
-													'rgba(255,99,132,1)',
-													'rgba(54, 162, 235, 1)',
-													'rgba(255, 206, 86, 1)',
-													'rgba(75, 192, 192, 1)',
-													'rgba(153, 102, 255, 1)',
-													'rgba(255, 159, 64, 1)'
-											],
-											borderWidth: 1
-									}]
-							},
-							options: {
-									scales: {
-											yAxes: [{
-													ticks: {
-															beginAtZero:true
-													}
-											}]
-									}
-							}
+				$('#sensorModal').modal('show').on('shown.bs.modal', function (event) {
+					var ctx = $("#sensorChart").get(0).getContext("2d");
+					var data = {
+						labels : ["1","2","4"],
+						datasets : [{
+							label: "",
+							fillColor: "rgba(151,187,205,0.2)",
+							strokeColor: "rgba(151,187,205,1)",
+							pointColor: "rgba(151,187,205,1)",
+							pointStrokeColor: "#fff",
+							pointHighlightFill: "#fff",
+							pointHighlightStroke: "rgba(151,187,205,1)",
+							data: [12,3,4]
+						}]
+					};
+					var gaugeChart = new Chart(ctx).Line(data, {bezierCurve:true, scaleLabel: "<%= ' ' + value%>"});
 					});
-				});
 				});
 			}
 	});
